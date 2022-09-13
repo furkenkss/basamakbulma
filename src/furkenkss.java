ae4baad9138fbddb8f6d4bf6f9c41590336dd6c6
furkenkss <beytullah.turhan@gmail.com>
2022-09-13 20:15:28 +0300

    done

---
+++ src/furkenkss.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class furkenkss {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+
+        System.out.print("Bir Sayı Girin: ");
+
+
+        int num = input.nextInt();
+
+        int adet = 0,toplam=0;
+
+        while(num != 0)
+        {
+            toplam +=  (num%10);
+            num /= 10;
+            ++adet;
+        }
+        System.out.println("basamak sayısı : " + adet);
+        System.out.println("basamak toplamı: " + toplam);
+    }
+}
